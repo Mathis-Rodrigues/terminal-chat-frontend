cb93c61ea6fd5ac104be5b5a5c709ea522e7cae2
Mathis-Rodrigues <mathis.rodrigues-de-oliveira@epitech.eu>
2022-12-30 14:16:10 +0100

    little change in lobby

--- src/Pages/Lobby.tsx
+++ src/Pages/Lobby.tsx
@@ -4,11 +4,13 @@ import { io, Socket } from 'socket.io-client';
 import { useTranslation } from 'react-i18next';
 import ChatBox from '../Components/ChatBox';
 import { Message } from '../Types/Message';
+import { Profile } from '../Types/Profile';
 
 function LobbyPage() {
   const [connected, setConnected] = useState(false);
   const [messages, setMessages] = useState<Message[]>([]);
   const [socket, setSocket] = useState<Socket | null>(null);
+  const [participants, setParticipants] = useState<{ user: Profile; socketId: string }[]>([]);
   const { t } = useTranslation();
   const { id } = useParams<{ id: string }>();
   const query = useMemo(() => new URLSearchParams(window.location.search), []);
@@ -30,36 +32,20 @@ function LobbyPage() {
     s.on('message', (m) => {
       setMessages((prev) => [...prev, m]);
     });
+    s.on('participants', (p) => {
+      console.log(p);
+      setParticipants(p);
+    });
     setSocket(s);
     return () => {
       s.disconnect();
+      setSocket(null);
     };
-  }, []);
-
-  // const socket = useMemo(() => {
-  //   const password = query.get('password');
-  //   const s = io(process.env.REACT_APP_SOCKETS_URL || '', {
-  //     auth: {
-  //       token: localStorage.getItem('token'),
-  //     },
-  //     query: {
-  //       room: id,
-  //       password,
-  //     },
-  //   });
-  //   s.on('join_ok', () => {
-  //     setConnected(true);
-  //   });
-  //   s.on('message', (m) => {
-  //     setMessages((prev) => [...prev, m]);
-  //   });
-  //   return s;
-  // }, [id, query]);
+  }, [id, query]);
 
   if (!socket) {
     return <p>{t('connexionHappenning')}</p>;
   }
-
   return (
     <div className="flex h-full w-full flex-col items-center">
       <div className="h-auto w-full max-w-xl border-2 border-primary bg-black shadow-cyber">
