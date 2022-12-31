475d5d335b984f57c2bf108425ca7f557c18be23
Mathis-Rodrigues <mathis.rodrigues-de-oliveira@epitech.eu>
2022-12-31 06:49:41 +0100

    add close modal on click outside

--- src/Components/Popup/index.tsx
+++ src/Components/Popup/index.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { faClose } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
@@ -15,35 +18,44 @@ function Popup({ isOpen, setIsOpen, onPasswordSubmit }: PopupProps) {
   const { t } = useTranslation();
 
   return (
-    <Modal
-      onBackdropClick={() => setIsOpen(false)}
-      show={isOpen}
-      onHide={() => setIsOpen(false)}
-      className="fixed top-0 left-0 right-0 bottom-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+    <div
+      onClick={(event) => {
+        if ((event.target as HTMLElement).id === 'modal') {
+          setIsOpen(false);
+        }
+      }}
     >
-      <div className="relative rounded-md border-2 border-primary bg-black p-5 lg:w-1/3 w-1/2">
-        <FontAwesomeIcon
-          className="absolute top-2 right-2 z-10 cursor-pointer text-primary md:text-xl"
-          icon={faClose}
-          onClick={() => setIsOpen(false)}
-        />
-        <input
-          placeholder={t('enterRoomPassword')}
-          className="w-full max-w-xl rounded-md bg-black px-2 py-2
+      <Modal
+        id="modal"
+        show={isOpen}
+        onHide={() => setIsOpen(false)}
+        onExit={() => setIsOpen(false)}
+        className="fixed top-0 left-0 right-0 bottom-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+      >
+        <div className="relative rounded-md border-2 border-primary bg-black p-5 lg:w-1/3 w-1/2">
+          <FontAwesomeIcon
+            className="absolute top-2 right-2 z-10 cursor-pointer text-primary md:text-xl"
+            icon={faClose}
+            onClick={() => setIsOpen(false)}
+          />
+          <input
+            placeholder={t('enterRoomPassword')}
+            className="w-full max-w-xl rounded-md bg-black px-2 py-2
               font-vt323 text-xl text-primary focus:outline-none"
-          onChange={(e) => setPasswordInput(e.target.value)}
-        />
-        <div className="flex items-center justify-center ">
-          <button
-            type="button"
-            className="mt-3 font-vt323 text-xl text-primary"
-            onClick={() => onPasswordSubmit(passwordInput)}
-          >
-            {t('join')}
-          </button>
+            onChange={(e) => setPasswordInput(e.target.value)}
+          />
+          <div className="flex items-center justify-center ">
+            <button
+              type="button"
+              className="mt-3 font-vt323 text-xl text-primary"
+              onClick={() => onPasswordSubmit(passwordInput)}
+            >
+              {t('join')}
+            </button>
+          </div>
         </div>
-      </div>
-    </Modal>
+      </Modal>
+    </div>
   );
 }
 
